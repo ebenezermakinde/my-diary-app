1de5b3169ddeecb9b5edea94e20acfae987bf383
ebenezermakinde <ebenezermakinde@yahoo.co.uk>
2019-03-29 14:03:31 +0100

    feat(my-diary-app): implement CRUD for diary
    - implement basic crud operation for the diary

---
+++ api/models/Entries.js
@@ -0,0 +1,22 @@
+/**
+ * Entries.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: 'string',
+      required: true,
+      minLength: 2
+    },
+    description: {
+      type: 'string',
+      required: true,
+      minLength: 8
+    }
+  },
+};
